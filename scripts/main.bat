1af83a54d541d15226290d96807d494852fa88ed
mo <jebaramo@gmail.com>
2024-05-31 17:35:29 +0100

    build scripts

---
+++ scripts/main.bat
@@ -0,0 +1,3 @@
+del main.obb.bak
+copy main.obb main.obb.bak
+c:\Qt\6.5.3\msvc2019_64\bin\rcc -binary ../main.qrc -o main.obb
---
+++ scripts/patch.bat
@@ -0,0 +1,3 @@
+del patch.obb.bak
+copy patch.obb patch.obb.bak
+c:\Qt\6.5.3\msvc2019_64\bin\rcc -binary ../patch.qrc -o patch.obb
