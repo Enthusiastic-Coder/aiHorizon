4125a627b1f74117d73623df03dfb60d7464c3ca
Mo <jebaramo@gmail.com>
2024-06-04 21:43:38 +0100

    Asset pack android fix

--- openglwindow.cpp
+++ openglwindow.cpp
@@ -18,98 +18,7 @@
 #include <QDir>
 #include <QResource>
 
-#ifdef Q_OS_ANDROID
-
-#include <QJniObject>
-#include <QJniEnvironment>
-
-
-struct AssetPackLocation {
-    QString assetPath;
-    int storage;
-    QString path;
-};
-
-
-struct AssetLocation {
-    QString path;
-    long offset =0l;
-    long size =0l;
-};
-
-AssetPackLocation getAssetPackLocation(const QString &assetPackName)
-{
-    QJniObject context = QNativeInterface::QAndroidApplication::context();
-    QJniObject assetHelper("com/enthusiasticcoder/aihorizon/AssetPackHelper",
-                           "(Landroid/content/Context;)V",
-                           context.object<jobject>());
-
-    QJniObject jAssetPackName = QJniObject::fromString(assetPackName);
-
-    QJniObject assetLocation = assetHelper.callObjectMethod("getPackLocation",
-                    "(Ljava/lang/String;)Lcom/google/android/play/core/assetpacks/AssetPackLocation;",
-                                                            jAssetPackName.object<jstring>());
-
-    AssetPackLocation location;
-    if (assetLocation.isValid())
-    {
-        location.assetPath = assetLocation.callObjectMethod("assetsPath", "()Ljava/lang/String;").toString();
-        location.path = assetLocation.callObjectMethod("path", "()Ljava/lang/String;").toString();
-        location.storage = assetLocation.callMethod<jint>("packStorageMethod", "()I");
-    }
-    else
-    {
-        location.path = "Failed " + assetPackName;
-    }
-
-    return location;
-}
-
-AssetLocation getAssetLocation(const QString &assetPackName, const QString &fileName) {
-    QJniObject context = QNativeInterface::QAndroidApplication::context();
-    QJniObject assetHelper("com/enthusiasticcoder/aihorizon/AssetPackHelper",
-                           "(Landroid/content/Context;)V",
-                           context.object<jobject>());
-
-    QJniObject jAssetPackName = QJniObject::fromString(assetPackName);
-    QJniObject jFileName = QJniObject::fromString(fileName);
-
-    QJniObject assetLocation = assetHelper.callObjectMethod("getAssetLocation",
-                                    "(Ljava/lang/String;Ljava/lang/String;)Lcom/google/android/play/core/assetpacks/AssetLocation;",
-                                                            jAssetPackName.object<jstring>(), jFileName.object<jstring>());
-
-    AssetLocation location;
-    if (assetLocation.isValid()) {
-        location.path = assetLocation.callObjectMethod("path", "()Ljava/lang/String;").toString();
-        location.offset = assetLocation.callMethod<jlong>("offset", "()J");
-        location.size = assetLocation.callMethod<jlong>("size", "()J");
-    }
-    else
-    {
-        location.path = "Failed to get asset location for" + assetPackName + "/" + fileName;
-    }
-
-    return location;
-}
-
-QByteArray getDataFromAsset(const QString &assetPackName, const QString &fileName)
-{
-    AssetLocation loc = getAssetLocation(assetPackName, fileName);
-
-    QByteArray data;
-
-    QFile file(loc.path);
-
-    if( file.open(QIODevice::ReadOnly))
-    {
-        file.seek(loc.offset);
-        data = file.read(loc.size);
-    }
-
-    return data;
-}
-
-#endif
+#include <jibbs/android/assetpack.h>
 
 QString checkAndAccessObbFiles(const QString &packName, const QString &assetName) {
 
@@ -145,20 +54,9 @@ OpenGLWindow::OpenGLWindow()
     _patchObbData = getDataFromAsset("patch","patch.obb");
     _extraObbData = getDataFromAsset("extra","extra.obb");
 #else
-    auto getData = [](QString filename) {
-
-        QFile file(filename);
-        if (file.open(QIODevice::ReadOnly))
-        {
-            return file.readAll();
-        }
-
-        return QByteArray{};
-    };
-
-    _mainObbData = getData("scripts/main.obb");
-    _patchObbData = getData("scripts/patch.obb");
-    _extraObbData = getData("scripts/extra.obb");
+    _mainObbData = getDataFromAsset("", "scripts/main.obb");
+    _patchObbData = getDataFromAsset("", "scripts/patch.obb");
+    _extraObbData = getDataFromAsset("", "scripts/extra.obb");
 #endif
 
     _messageList << QString("Null:%1").arg(_mainObbData.isNull()?"Yes":"No");
