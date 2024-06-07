5d0b76d00abfa159029679f4dccacb2613f05a2c
mo <jebaramo@gmail.com>
2024-06-07 16:22:24 +0100

    policy update

--- QTopWindow.cpp
+++ QTopWindow.cpp
@@ -18,7 +18,7 @@ QTopWindow::QTopWindow(QWidget *parent)
 
     connect( ui->actionPrivacy_Policy, &QAction::triggered, this, [this](bool checked) {
 
-        QMessageBox::information(this, "Privacy Policy", "Please visit https://x.com/ADSBFlightTrkr/status/1798674939975025010");
+        QMessageBox::information(this, "Privacy Policy", "Please visit https://enthusiastic-coder.github.io/aihorizon-privacy-policy/");
     });
 }
 
